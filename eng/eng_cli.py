a5d09d4949f637cdea0c90cba0785414b9673779
William Dussault <dalloriam@gmail.com>
2016-10-15 14:44:43 -0400

    CLI tool refactor (#54)
    * Start refactoring eng
    
    * Add resource folder creation
    
    * Cleanup eng

---
+++ eng/eng_cli.py
@@ -0,0 +1,28 @@
+import sys
+from eng.logging import info, error
+
+from eng.generator import generate
+from eng.scaffolder import new
+
+
+invalid = "Invalid action."
+valid_actions = {'generate': generate, 'new': new}
+
+
+def cli(options):
+  if not options:
+    error(invalid)
+    return
+
+  action = options[0]
+
+  for k in valid_actions:
+    if action == k or action == k[0]:
+      valid_actions[k](options[1:])
+      return
+  error(invalid)
+
+def main():
+  cli(sys.argv[1:])
+  info("Done.")
+
---
+++ eng/scaffolder/__init__.py
@@ -0,0 +1,1 @@
+from .new import new
