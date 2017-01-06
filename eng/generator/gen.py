de05581e7de165634c0664b2fa5bdf799c1bca40
William Dussault <dalloriam@gmail.com>
2017-01-06 15:16:04 -0500

    Fix PEP8

--- eng/generator/gen.py
+++ eng/generator/gen.py
@@ -1,33 +1,37 @@
-from eng.logging import info, error, success
+from eng.logging import error
 from eng.utils import render_view, write_file, render_service
 
 import os
 
-def make_view(view_name):
-  if not os.path.isdir('views'):
-    error("eng generate must be called from the root directory of an engel application.")
 
-  write_file(os.path.join(os.path.abspath('views'), view_name + '.py'), render_view(view_name))
+def make_view(view_name):
+    if not os.path.isdir('views'):
+        error("eng generate must be called from the root directory of an engel application.")
+
+    write_file(os.path.join(os.path.abspath('views'), view_name + '.py'), render_view(view_name))
+
 
 def make_service(svc):
-  if not os.path.isdir('services'):
-    error("eng generate must be called from the root directory of an engel application.")
+    if not os.path.isdir('services'):
+        error("eng generate must be called from the root directory of an engel application.")
 
-  write_file(os.path.join(os.path.abspath('services'), svc + '.py'), render_service(svc))
+    write_file(os.path.join(os.path.abspath('services'), svc + '.py'), render_service(svc))
+
 
 valid_options = {'view': make_view, 'service': make_service}
 
+
 def generate(options):
 
-  if len(options) != 2:
-    error("Invalid Syntax.")
+    if len(options) != 2:
+        error("Invalid Syntax.")
 
-  target = options[0]
-  name = options[1]
+    target = options[0]
+    name = options[1]
 
-  for key in valid_options:
-    if key == target or target == key[0]:
-      valid_options[key](name)
-      return
+    for key in valid_options:
+        if key == target or target == key[0]:
+            valid_options[key](name)
+            return
 
-  error("Invalid Operation.")
+    error("Invalid Operation.")
--- engel/websocket.py
+++ engel/websocket.py
@@ -13,126 +13,137 @@ from autobahn.asyncio.websocket import WebSocketServerFactory, WebSocketServerPr
 
 class EventProcessor(object):
 
-  def __init__(self):
-    self.handlers = defaultdict(lambda: defaultdict(lambda: []))
+    def __init__(self):
+        self.handlers = defaultdict(lambda: defaultdict(lambda: []))
 
-  def register(self, event, callback, selector=None):
-    print('Registering: ' + str(event))
+    def register(self, event, callback, selector=None):
+        print('Registering: ' + str(event))
 
-    if selector:
-      key = str(id(callback))
-    else:
-      key = '_'
+        if selector:
+            key = str(id(callback))
+        else:
+            key = '_'
 
-    self.handlers[event][key].append(callback)
+        self.handlers[event][key].append(callback)
 
-    if event not in ('init', 'load', 'close'):
-      capture = False
-      if selector is None:
-        selector = 'html'
-        capture = True
+        if event not in ('init', 'load', 'close'):
+            capture = False
+            if selector is None:
+                selector = 'html'
+                capture = True
 
-      print('Dispatching: ' + str(event))
-      self.dispatch({'name': 'subscribe', 'event': event, 'selector': selector, 'capture': capture, 'key': str(id(callback))})
+            print('Dispatching: ' + str(event))
+            self.dispatch({
+                'name': 'subscribe',
+                'event': event,
+                'selector': selector,
+                'capture': capture,
+                'key': str(id(callback))
+            })
 
-  def unregister(self, event, callback, selector=None):
-    if event not in self.handlers:
-      return
+    def unregister(self, event, callback, selector=None):
+        if event not in self.handlers:
+            return
 
-    if selector is None:
-      self.handlers[event]['_'].remove(callback)
-      if not self.handlers[event]['_']:
-        self.handlers[event].pop('_')
-    else:
-      self.handlers[event].pop(str(id(callback)))
+        if selector is None:
+            self.handlers[event]['_'].remove(callback)
+            if not self.handlers[event]['_']:
+                self.handlers[event].pop('_')
+        else:
+            self.handlers[event].pop(str(id(callback)))
 
-    if not self.handlers[event]:
-      self.handlers.pop(event)
+        if not self.handlers[event]:
+            self.handlers.pop(event)
 
-    if event not in ('init', 'load', 'close'):
-      self.dispatch({'name': 'unsubscribe', 'event': event, 'selector': selector, 'key': str(id(callback))})
+        if event not in ('init', 'load', 'close'):
+            self.dispatch({'name': 'unsubscribe', 'event': event,
+                           'selector': selector, 'key': str(id(callback))})
 
-  def dispatch(self, command):
-    self.protocol.sendMessage(bytes(json.dumps(command), 'utf-8'), False)
+    def dispatch(self, command):
+        self.protocol.sendMessage(bytes(json.dumps(command), 'utf-8'), False)
 
-  @asyncio.coroutine
-  def process(self, protocol, event):
-    self.protocol = protocol
-    event_type = event['event']
+    @asyncio.coroutine
+    def process(self, protocol, event):
+        self.protocol = protocol
+        event_type = event['event']
 
-    if event_type in self.handlers:
-      if 'key' in event:
-        key = event['key']
-        if key in self.handlers[event_type]:
-          for handler in self.handlers[event_type][key]:
-            if callable(handler):
-              command = yield from handler(event, self)
+        if event_type in self.handlers:
+            if 'key' in event:
+                key = event['key']
+                if key in self.handlers[event_type]:
+                    for handler in self.handlers[event_type][key]:
+                        if callable(handler):
+                            command = yield from handler(event, self)
 
-              if command:
-                self.dispatch(command)
+                            if command:
+                                self.dispatch(command)
 
-      for handler in self.handlers[event_type]['_']:
-        if callable(handler):
-          command = yield from handler(event, self)
-          if command:
-            self.dispatch(command)
+            for handler in self.handlers[event_type]['_']:
+                if callable(handler):
+                    command = yield from handler(event, self)
+                    if command:
+                        self.dispatch(command)
 
 
 class EventProtocol(WebSocketServerProtocol):
 
-  def onConnect(self, request):
-    logging.info("Client connecting: %s" % request.peer)
+    def onConnect(self, request):
+        logging.info("Client connecting: %s" % request.peer)
 
-  def onOpen(self):
-    logging.info("WebSocket connection open")
+    def onOpen(self):
+        logging.info("WebSocket connection open")
 
-  @asyncio.coroutine
-  def onMessage(self, payload, isBinary):
-    if isBinary:
-      logging.info("Binary message received (NOT SUPPORTED YET): {} bytes".format(len(payload)))
-    else:
-      logging.info("Text message received: {}".format(payload.decode('utf-8')))
-      body = json.loads(payload.decode('utf-8'))
-      if 'event' in body:
-        yield from self.processor.process(self, body)
+    @asyncio.coroutine
+    def onMessage(self, payload, isBinary):
+        if isBinary:
+            logging.info(
+                "Binary message received: {} bytes".format(len(payload))
+            )
+        else:
+            logging.info("Text message received: {}".format(
+                payload.decode('utf-8')))
+            body = json.loads(payload.decode('utf-8'))
+            if 'event' in body:
+                yield from self.processor.process(self, body)
 
-  def onClose(self, wasClean, code, reason):
-    logging.info("WebSocket connection closed: {}".format(reason))
-    sys.exit(0)
+    def onClose(self, wasClean, code, reason):
+        logging.info("WebSocket connection closed: {}".format(reason))
+        sys.exit(0)
 
 
 class EventServer(object):
 
-  def __init__(self, hostname="localhost", port="8080", processor=None):
-    self.hostname = hostname
-    self.port = port
-    self.processor = processor
+    def __init__(self, hostname="localhost", port="8080", processor=None):
+        self.hostname = hostname
+        self.port = port
+        self.processor = processor
 
-    factory = WebSocketServerFactory(u"ws://" + hostname + u":" + str(port))
+        factory = WebSocketServerFactory(
+            u"ws://" + hostname + u":" + str(port))
 
-    protocol = EventProtocol
-    protocol.processor = processor
-    protocol.app = self
+        protocol = EventProtocol
+        protocol.processor = processor
+        protocol.app = self
 
-    factory.protocol = protocol
+        factory.protocol = protocol
 
-    self.loop = asyncio.get_event_loop()
-    self.server = self.loop.create_server(factory, '0.0.0.0', port)
+        self.loop = asyncio.get_event_loop()
+        self.server = self.loop.create_server(factory, '0.0.0.0', port)
 
-  def start(self, callback=None):
-    self.loop.run_until_complete(self.server)
+    def start(self, callback=None):
+        self.loop.run_until_complete(self.server)
 
-    try:
-      path = os.path.dirname(os.path.realpath(__file__))
-      webbrowser.open('file:///' + os.path.join(path, 'index.html'))
-      self.loop.run_forever()
+        try:
+            path = os.path.dirname(os.path.realpath(__file__))
+            webbrowser.open('file:///' + os.path.join(path, 'index.html'))
+            self.loop.run_forever()
 
-    except KeyboardInterrupt:
-      pass
+        except KeyboardInterrupt:
+            pass
 
-    finally:
-      self.server.close()
-      self.loop.close()
+        finally:
+            self.server.close()
+            self.loop.close()
 
-      if callback is not None and callable(callback):
-        callback()
+            if callback is not None and callable(callback):
+                callback()
